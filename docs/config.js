7d26aec0bd98dbae94065a70116cc052dde076a9
Your Name <you@example.com>
2026-08-28 16:02:58 -0400

    actualizacion nombre carpeta

--- docs/config.js
+++
@@ -1,4 +0,0 @@
-// Config local, NO se sube a git (ver .gitignore).
-// Copia estos valores desde tu archivo .env de la raíz del proyecto.
-window.SUPABASE_URL = "https://usqboirxjxxrxjcnjrsf.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzcWJvaXJ4anh4cnhqY25qcnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyODQyNjMsImV4cCI6MjEwMDg2MDI2M30.jPC6tzv8t9UD52GnGxyxid-swkMFCdaUgJ65S1I2iJ8";
